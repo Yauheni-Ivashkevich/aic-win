08948a679bca1e7a7228cd68da89ff1e2f155b55
Yauheni Ivashkevich <zheklex@ya.ru>
2021-09-12 12:36:57 +0300

    Added pymongo

--- app/main.py
+++ app/main.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask_pymongo import PyMongo
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
+
+mongodb_client = PyMongo(
+    app,
+    uri=
+    "mongodb+srv://eugene_ivashkevich:1211@mongo-heroku-cluster.gi7cs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+)
+db = mongodb_client.db
+
+
+@app.route("/add_one")
+def add_one():
+    db.todos.insert_one({'title': "todo title", 'body': "todo body"})
+    return flask.jsonify(message="success")
+
 
 @app.route("/")
 def home_view():
-        return "<h1>Welcome to Geeks for Geeks</h1>"
+    return "<h1>Welcome to Geeks for Geeks</h1>"
