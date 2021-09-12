fde00d32c5c933fd0da79fce64a1b6f560a8caba
Yauheni Ivashkevich <zheklex@ya.ru>
2021-09-12 20:52:55 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from flask_pymongo import PyMongo
 from flask import Flask, request, jsonify
+from pymongo.errors import BulkWriteError
 
 app = Flask(__name__)
 
@@ -15,6 +16,26 @@ db = mongodb_client.db
 def add_one():
     db.todos.insert_one({'title': "todo title", 'body': "todo body"})
     return jsonify(message="success")
+
+
+@app.route("/add_many")
+def add_many():
+    try:
+        todo_many = db.todos.insert_many([
+            {'_id': 1, 'title': "todo title one ", 'body': "todo body one "},
+            {'_id': 8, 'title': "todo title two", 'body': "todo body two"},
+            {'_id': 2, 'title': "todo title three", 'body': "todo body three"},
+            {'_id': 9, 'title': "todo title four", 'body': "todo body four"},
+            {'_id': 10, 'title': "todo title five", 'body': "todo body five"},
+            {'_id': 5, 'title': "todo title six", 'body': "todo body six"},
+        ], ordered=False)
+    except BulkWriteError as e:
+        return jsonify(message="duplicates encountered and ignored",
+                             details=e.details,
+                             inserted=e.details['nInserted'],
+                             duplicates=[x['op'] for x in e.details['writeErrors']])
+
+    return jsonify(message="success", insertedIds=todo_many.inserted_ids)
 
 
 @app.route("/test", methods=['GET', 'POST'])
